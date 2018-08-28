fd496005d54f7fad4a89fecf21cbf8732138cfe5
Aaron Duryee <kalidrev@gmail.com>
2018-08-28 15:44:20 -0500

    Showcase removal of magic numbers and use of nullable types

--- GameConsole/PlayerCharacter.cs
+++ GameConsole/PlayerCharacter.cs
@@ -9,14 +9,14 @@ namespace GameConsole
     class PlayerCharacter
     {
         public string Name { get; set; }
-        public int DaysSinceLastLogin { get; set; }
+        public int? DaysSinceLastLogin { get; set; }
+        public bool? IsNoob { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public PlayerCharacter()
         {
-            DateOfBirth = DateTime.MinValue; //magic number
-            DaysSinceLastLogin = -1; //magic number
+           
         }
     }
 }
--- GameConsole/PlayerDisplayer.cs
+++ GameConsole/PlayerDisplayer.cs
@@ -10,24 +10,44 @@ namespace GameConsole
     {
         public static void Write(PlayerCharacter player)
         {
-            Console.WriteLine(player.Name);
-
-            if(player.DaysSinceLastLogin == -1)
+            if (string.IsNullOrWhiteSpace(player.Name))
             {
-                Console.WriteLine("No value for DaysSinceLastLogin");
+                Console.WriteLine("Player name is null or all whitespace");
             }
             else
             {
-                Console.WriteLine(player.DaysSinceLastLogin);
+                Console.WriteLine(player.Name);
             }
 
-            if(player.DateOfBirth == DateTime.MinValue)
+            if(player.DaysSinceLastLogin.HasValue)
+            {
+                Console.WriteLine(player.DaysSinceLastLogin.Value);
+            }
+            else
+            {
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            }
+
+            if(player.DateOfBirth == null)
             {
                 Console.WriteLine("No date of birth specified");
             }
             else
             {
                 Console.WriteLine(player.DateOfBirth);
+            }
+
+            if(player.IsNoob == null)
+            {
+                Console.WriteLine("Player newbie status is unknown");
+            }
+            else if (player.IsNoob == true)
+            {
+                Console.WriteLine("Player is a newbie");
+            }
+            else
+            {
+                Console.WriteLine("Player is experienced");
             }
         }
     }
