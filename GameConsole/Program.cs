52c553ac0e09835ce09c959ee4ec0d1e4c3e0078
Aaron Duryee <kalidrev@gmail.com>
2018-08-28 00:43:06 -0500

    Showcase the use of "magic numbers" to represent null

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -10,6 +10,14 @@ namespace GameConsole
     {
         static void Main(string[] args)
         {
+            var player = new PlayerCharacter();
+
+            player.Name = "Sarah";
+            //player.DaysSinceLastLogin = 42;
+
+            PlayerDisplayer.Write(player);
+
+            Console.ReadLine();
         }
     }
 }
